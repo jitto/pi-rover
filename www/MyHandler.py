76bc6edbf6b2290f74202c71a0339583f7950cab
nikhil.jt <pi@raspberrypi.(none)>
2014-01-12 22:34:55 +0000

    awesomeness

--- www/MyHandler.py
+++ www/MyHandler.py
@@ -4,6 +4,20 @@ import RPi.GPIO as GPIO
 
 class MyHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
 
+  def motorTurn(self, pin8, pin10, pin16, pin18):
+    GPIO.output(16,pin16)
+    GPIO.output(18, pin18)
+    GPIO.output(8,pin8)
+    GPIO.output(10, pin10)
+      
+  def __init__(self, request, client_address, server):
+    SimpleHTTPServer.SimpleHTTPRequestHandler.__init__(self, request, client_address, server)
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(16, GPIO.OUT)
+    GPIO.setup(18, GPIO.OUT)
+    GPIO.setup(8, GPIO.OUT)
+    GPIO.setup(10, GPIO.OUT)
+  
   def do_GET(self):
 
     parsedParameters = urlparse.urlparse(self.path)
@@ -16,18 +30,17 @@ class MyHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
 
   def processMyRequest(self, query):
     print query
-    if ( 'forward' in self.path ):
-      GPIO.setmode(GPIO.BOARD)
-      GPIO.setup(16, GPIO.OUT)
-      GPIO.output(16,True)
-      GPIO.setup(18, GPIO.OUT)
-      GPIO.output(18, False)
-    else:
-      GPIO.setmode(GPIO.BOARD)
-      GPIO.setup(16, GPIO.OUT)
-      GPIO.output(16,False)
-      GPIO.setup(18, GPIO.OUT)
-      GPIO.output(18, True)
- 
     
- 
+    if ( 'forward' in self.path ):            
+      self.motorTurn(True, False, True, False)
+    elif ( 'backward' in self.path ):     
+      self.motorTurn(False, True, False, True)
+    elif ( 'left' in self.path ):
+      self.motorTurn(False, True, True, False)
+    elif ( 'right' in self.path):
+      self.motorTurn(True, False, False, True)
+    elif ( 'stop' in self.path):
+      self.motorTurn(False, False, False, False)
+      
+  
+      
