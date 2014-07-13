d956712320dc3070b3827af794b512d9ebc77ead
jitto <justine.vt@gmail.com>
2014-07-13 00:12:01 +0000

    read eeprom containing filament info

---
+++ 3dPrint/Unio.py
@@ -0,0 +1,87 @@
+import time
+import RPi.GPIO as GPIO
+
+def usleep(microSeconds):
+  time.sleep(microSeconds/1000000)
+
+class Unio:
+  UNIO_STARTHEADER = 0x55
+  UNIO_READ = 0x03
+  UNIO_RDSR = 0x05
+  UNIO_TSS = 10
+  UNIO_THDR = 5
+  UNIO_TSTBY = 600
+  UNIO_FUDGE_FACTOR = 5
+  UNIO_QUARTER_BIT = 10
+  gpioPin = 18
+  addr = 0xa0
+
+  def __init__(self):
+    GPIO.setmode(GPIO.BCM)
+
+  def readBytes(self, start, length):
+    self.standbyPulse()
+    self.startHeader()
+    self.send([self.addr, self.UNIO_READ, start >> 8, start & 0xff], False)
+    return self.read(length)
+
+  def standbyPulse(self):
+    self.setBus(0)
+    self.setOutput()
+    usleep(self.UNIO_TSS + self.UNIO_FUDGE_FACTOR)
+    self.setBus(1)
+    usleep(self.UNIO_TSTBY + self.UNIO_FUDGE_FACTOR)
+
+  def startHeader(self):
+    self.setBus(0)
+    usleep(self.UNIO_THDR + self.UNIO_FUDGE_FACTOR)
+    self.send([self.UNIO_STARTHEADER], True)
+
+  def send(self, bytes, end):
+    for byteIndex in range (0, len(bytes)):
+      byte = bytes[byteIndex]
+      for bitIndex in range(0, 8):
+        self.rwbit(byte & 0x80)
+        byte << 1
+      self.rwbit(end and ((byteIndex + 1) == len(bytes)))
+    return self.readBit()
+
+  def read(self, length):
+    result = []
+    for byteIndex in range (0, length):
+      data = 0
+      self.setInput()
+      for bitIndex in range (0, 8):
+        data = (data << 1) | self.rwbit(1)
+      self.setOutput()
+      self.readBit()
+      result.append(data)
+
+  def rwbit(self, bit):
+    self.setBus(not bit)
+    usleep(self.UNIO_QUARTER_BIT)
+    a = self.readBus();
+    usleep(self.UNIO_QUARTER_BIT)
+    self.setBus(bit)
+    usleep(self.UNIO_QUARTER_BIT)
+    b = self.readBus()
+    usleep(self.UNIO_QUARTER_BIT)
+    return b and not a
+
+  def readBit(self):
+    self.setInput()
+    b = self.rwbit(1)
+    self.setOutput()
+    return b
+
+  def readBus(self):
+    return GPIO.input(17) 
+
+  def setBus(self, state):
+    GPIO.output(17, state)
+
+  def setOutput(self):
+    GPIO.setup(17, GPIO.OUT)
+
+  def setInput(self):
+    GPIO.setup(17, GPIO.IN)
---
+++ 3dPrint/filament.py
@@ -0,0 +1,4 @@
+from Unio import Unio
+unio = Unio()
+bytes = unio.readBytes(0, 128)
+print bytes
